f1fbad57b711d8d9208c97f8cee5d865f8ea68a9
aleksandar.kgeorgiev <aleksandar.kgeorgiev@scalefocus.com>
2024-12-30 18:07:53 +0200

    capturing images from Camera

--- app/src/main/java/com/sales/imagepicker/MainActivity.java
+++ app/src/main/java/com/sales/imagepicker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sales.imagepicker;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -24,9 +28,11 @@ public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
+
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK){
+
+                    if (result.getResultCode() == RESULT_OK) {
                         Uri image_uri = result.getData().getData();
                         imageView.setImageURI(image_uri);
                     }
@@ -52,20 +58,63 @@ public class MainActivity extends AppCompatActivity {
         galleryBtn = findViewById(R.id.button);
         cameraBtn = findViewById(R.id.button2);
 
+        //TODO ask for permission of camera upon first launch of application
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            String[] permission = {Manifest.permission.CAMERA};
+            requestPermissions(permission, 112);
+        }
 
-        galleryBtn.setOnClickListener( new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    galleryActivityResultLauncher.launch(galleryIntent);
-                }
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
+            }
         });
 
-        cameraBtn.setOnClickListener( new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    String[] permission = {Manifest.permission.CAMERA};
+                    requestPermissions(permission, 112);
+                } else {
+                    openCamera();
+                }
             }
         });
     }
+
+    Uri image_uri;
+
+    //TODO opens camera so that user can capture image
+    private void openCamera() {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        cameraActivityResultLauncher.launch(cameraIntent);
+    }
+
+    //TODO capture the image using camera and display it
+    ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == RESULT_OK) {
+                        imageView.setImageURI(image_uri);
+                    }
+                }
+            });
+
 }
